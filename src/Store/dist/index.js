d77e9cef2ba532fc27cb34c1366dd5a22f660c7f
Nicolas Trote <nicolas.trote@gmail.com>
2021-02-04 17:35:51 -0500

    delete unwanted dist folders

--- src/Store/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.reducer = void 0;
-var redux_1 = require("redux");
-var reducer_1 = require("./BreweryStore/reducer");
-exports.reducer = redux_1.combineReducers({
-    root: reducer_1["default"]
-});
